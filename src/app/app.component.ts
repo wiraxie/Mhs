ffbdf76e512a9ac885c70c8763946951b63ce795
WiraXie <wiraxie.wira_xie@ymail.com>
2017-10-26 11:18:32 +0700

    tambahan di component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { NgIf } from '@angular/common';
 
 export class AppComponent 
 {
-  title = 'ini tengah';
+  title = 'table data mhs';
     // public modalRef: BsModalRef;
     // constructor(private modalService: BsModalService) {}
 
@@ -30,15 +30,25 @@ export class AppComponent
   public telp: number;
   public mhs: Array<{id: number, nama: string, angkatan: number, jurusan: string, alamat: string, email: string, telp: number}> = [];
 
-  selectedMhs;
+  selectedId = this.id;
 
   addData() 
   {
     this.mhs.push( {id: this.id, nama: this.nama, angkatan: this.angkatan, jurusan: this.jurusan, alamat:this.alamat, email: this.email, telp: this.telp } );
   }
 
+  //penampung
+  public newId;
+  public newNama;
+  public newAngkatan;
+  public newJurusan;
+  public newAlamat;
+  public newEmail;
+  public newTelp;
+  public newMhs: Array<{newId: number, newNama: string, newAngkatan: number, newJurusan: string, newAlamat: string, newEmail: string, newTelp: number}> = [];
+  
   editData(id): void
   {
-    
+    this.selectedId;
   }
 }
